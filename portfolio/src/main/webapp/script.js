bf7ff4238291a81001581a05e7aa651a2aedac48
Tengku Hana Shafikah <tghanashafikah@gmail.com>
2020-06-24 13:35:11 -0400

    Add functions that create a list element containing comment data and builds the UI.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,8 +1,35 @@
 /**
- * Shows the following greeting: "Hello Hana!"
+ * Fetches the comments and builds the UI.
  */
-async function getGreeting() {
-  const response = await fetch('/data');
-  const greeting = await response.text();
-  document.getElementById('greeting-container').innerText = greeting;
+function getComments() {
+  fetch("/data")
+    .then(response => response.json())
+    .then(comments => {
+      const commentsListElement = document.getElementById("comments-list");
+      commentsListElement.innerHTML = "";
+      comments.forEach(line => {
+        commentsListElement.appendChild(createListElement(line));
+      });
+    });
+}
+
+/** Creates an <li> element containing comment data.*/
+function createListElement(comment) {
+  const commentElement = document.createElement('li');
+  commentElement.className = 'comment';
+
+  const nameElement = document.createElement('b');
+  nameElement.innerText = comment.name + ": ";
+
+  const textElement = document.createElement('span');
+  textElement.innerText = comment.text;
+
+  const timeElement = document.createElement('span');
+  timeElement.innerText = "   (" + comment.date + ")";
+  
+  commentElement.appendChild(nameElement);
+  commentElement.appendChild(textElement);
+  commentElement.appendChild(timeElement);
+
+  return commentElement;
 }
